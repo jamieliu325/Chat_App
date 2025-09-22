6334a8c40fb0430143d77006f7a5783c2392ef29
Zhipeng Liu <zliu352@gatech.edu>
2025-09-22 10:23:11 -0400

    update run code

--- website/application/main.py
+++ website/application/main.py
@@ -32,7 +32,7 @@ def handle_my_custom_event(json):
 # --- Run app ---
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     socketio.run(
         app,
         host="0.0.0.0",
